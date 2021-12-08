12ebe984bff4050a98a8a78e00069a22dcb72636
Ramzi Osta <ramziosta@Ramzis-Mac-mini.local>
2021-12-08 16:56:05 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Button, Input, InputLabel, FormControl} from "@material-ui/core";
+import "./App.css";
+import Todo from "./Todo"
 
 function App() {
+  //state for the todo list
+  const [todos, setTodos] = React.useState(["walk Leo","walk Lucy","walk The Dogs"]);
+  // state for the new todo item
+  const [input, setInput] = React.useState("");
+  console.log("🟥 " + input);
+
+  //adds the new list item to the list
+  const addTodo = (e) => {
+    //prevents page refresh when button is submited and deletes the state memory
+    e.preventDefault();
+    setTodos([...todos, input]);
+    //clears the input field
+    setInput("");
+  };
+    const addNewTodo = (e)=> setInput(e.target.value)
+    
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>ToDo List</h1>
+  {/* the form added to allow the button to submit when hit enter */}
+      <form>
+          <FormControl>
+            <InputLabel>🔳 Add a Todo</InputLabel>
+            <Input value={input} label="Outlined primary" onChange={addNewTodo} />
+          </FormControl>
+
+          {/* disabled: disable adding null value */}
+          <Button variant="contained" disabled={!input} color="primary" type="submit" onClick={addTodo} >Add ToDos</Button>
+      </form>
+
+      <ul>
+        {
+          todos.map(todo => (
+            <Todo text={todo} />
+              
+            ))}
+      </ul>
     </div>
   );
 }
 
 export default App;
+
+ 
